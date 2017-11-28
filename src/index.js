4f1a77786bb463ebc84fcda473262bf7e9588883
jasmine <jasliu2012@gmail.com>
2017-11-28 16:53:25 -0600

    Added location to render the DOMon index.js through document.querySelector

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a new component. This should produce some HTML
-const App = function() {
+const App = () => {
   return <div>Exploding Kittens</div>
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
