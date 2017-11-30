3ecf33a0e10d42295a17e161fd2c6a624ad73ebb
jasmine <jasliu2012@gmail.com>
2017-11-30 13:26:14 -0600

    declared new component SearchBar and exported it. Then imported it to index.js and rendered in App component, which is rendered in the div in index.html class container.

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return <input />
+};
+
+export default SearchBar;
+
+
+// declared new component SearchBar and exported it. Then imported it to index.js and rendered in App component, which is rendered in the div in index.html class container.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import key from './youtubeKey';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import key from './youtubeKey';
 
 // Create a new component. This should produce some HTML
 const App = () => {
-  return <div>Exploding Kittens</div>
+  return (
+    <div>
+      <SearchBar />  
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
