470191b2bfbfb2c156ee27e8905635c0e408451f
jasmine <jasliu2012@gmail.com>
2017-12-01 14:31:52 -0600

    Made App into class component with state

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import key from './youtubeKey';
 import YTSearch from 'youtube-api-search';
 
-YTSearch({key: key, term: 'kittens'}, function(data) {
-  console.log(data);
-});
-
 // Create a new component. This should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />  
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: key, term: 'kittens'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />  
+      </div>
+    );
+  };
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
