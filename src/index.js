6ee850a2c50e80a97bdb393bc45634d0db3b6137
jasmine <jasliu2012@gmail.com>
2017-12-01 14:18:15 -0600

    Added YTSearch configuration function and callback function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import key from './youtubeKey';
+import YTSearch from 'youtube-api-search';
+
+YTSearch({key: key, term: 'kittens'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This should produce some HTML
 const App = () => {
